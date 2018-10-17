8e54690b592f27b8c867b6d3c149f6f2f27fc749
Chad Allen <chadvallen@gmail.com>
2018-10-16 17:04:59 -0700

    Finished afternoon

---
+++ server/controllers/auth_controller.js
@@ -0,0 +1,36 @@
+const users = require('../models/users');
+let id = 1;
+
+module.exports = {
+    login: (req, res, next) => {
+        const { username, password } = req.body;
+        const { session } = req;
+        const user = users.find( user => user.username === username && user.password === password);
+        if ( user ) {
+            session.user.username = user.username;
+            res.status(200).send(session.user);
+        } else {
+            res.sendStatus(500);
+        }
+    },
+
+    register: (req, res, next) => {
+        const { username, password } = req.body;
+        const { session } = req;
+        users.push({ id, username, password })
+        id++;
+        session.user.username = username;
+        res.status(200).send(session.user);
+    },
+
+    signout: (req, res, next) => {
+        const { session } = req
+        session.destroy()
+        res.status(200).send(req.session);
+    },
+
+    getUser: (req, res, next) => {
+        const { session } = req;
+        res.status(200).send(session.user);
+    }
+}
